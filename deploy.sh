468752011001d705c47dde8e2ae52cb399ff0718
vagrant <vagrant@master-01>
2019-12-03 12:04:49 +0000

    :wheel_of_dharma: commit from master1

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+kubectl apply -f frontend-deployment.yaml,frontend-svc.yaml
+kubectl apply -f order-deployment.yaml,order-svc.yaml
+kubectl apply -f product-deployment.yaml,product-svc.yaml
+kubectl apply -f user-deployment.yaml,user-svc.yaml
+
